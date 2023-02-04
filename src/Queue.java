bc7710de40846ed7c99a96a544989e2a77ff7d80
AlexaGTorres <alexa.torres122694@gmail.com>
2023-02-03 20:21:03 -0800

    I moved the arrangement of the code, put it in the wrong place.

--- src/Queue.java
+++ src/Queue.java
@@ -4,6 +4,45 @@ public class Queue
     private Node head;
     private Node tail;
 
+    public boolean isEmpty()
+    {
+        return head == null;
+    }
+
+    public int peek ()
+    {
+        return head.data;
+    }
+
+    public void add (int data)
+    {
+        Node node = new Node(data);
+
+        if (tail != null)
+        {
+            tail.next = node;
+        }
+
+        tail = node;
+
+        if (head == null)
+        {
+            head = node;
+        }
+    }//End add method
+
+    public int remove ()
+    {
+        int data = head.data;
+        head = head.next;
+
+        if (head == null)
+        {
+            tail = null;
+        }
+        return data;
+    }//End remove method
+
     //The other class, yes
     private class Node
     {
@@ -16,45 +55,6 @@ public class Queue
         {
             this.data = data;
         }
-
-        public boolean isEmpty()
-        {
-            return head == null;
-        }
-
-        public int peek ()
-        {
-            return head.data;
-        }
-
-        public void add (int data)
-        {
-            Node node = new Node(data);
-
-            if (tail != null)
-            {
-                tail.next = node;
-            }
-
-            tail = node;
-
-            if (head == null)
-            {
-                head = node;
-            }
-        }//End add method
-
-        public int remove ()
-        {
-            int data = head.data;
-            head = head.next;
-
-            if (head == null)
-            {
-                tail = null;
-            }
-            return data;
-        }//End remove method
 
         //toString
         @Override
