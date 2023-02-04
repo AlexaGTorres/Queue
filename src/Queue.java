caae08769cfbd74dff8928ecc98a56684df2c6f4
AlexaGTorres <alexa.torres122694@gmail.com>
2023-02-03 19:50:45 -0800

    Added variables and created Node class.

--- src/Queue.java
+++ src/Queue.java
@@ -1,3 +1,31 @@
 public class Queue
 {
+    //Variables
+    private int head;
+    private Node tail;
+
+    //Constructor
+
+    //Output method
+
+
+    //The other class, yes
+    private class Node
+    {
+        //Variables
+        private int data;
+        private Node next;
+
+        //Constructor
+        private Node (int data)
+        {
+            this.data = data;
+        }
+
+        //Getters and setters
+
+        //toString
+
+    }//End Node class
+
 }//End class Queue
